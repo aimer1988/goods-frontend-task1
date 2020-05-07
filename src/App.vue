cc7cafc5b13de2211761f1ee87d5208ea0e82c72
Alexander Marin <aimer1988@mail.ru>
2020-05-07 15:42:59 +0300

    and another one commit

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,46 @@
 <template>
   <div id="app">
-    <span>Адрес:</span>
-    <search-input
-      :search-function="searchFunctionsList.getAddressSuggestions"
-      @value-selected="address = $event"
-    />
+    <div class="address">
+      <span>Адрес:</span>
+      <search-input
+        :search-function="searchFunctionsList.getAddressSuggestions"
+        @value-selected="address = $event"
+      />
 
-    <div v-if="address">
-      <p>
-        <span>Страна:</span> <span>{{ address.country }}</span>
-      </p>
-      <p>
-        <span>Город:</span> <span>{{ address.city }}</span>
-      </p>
-      <p>
-        <span>Улица:</span> <span>{{ address.street }}</span>
-      </p>
-      <p>
-        <span>Дом:</span> <span>{{ address.house }}</span>
-      </p>
-      <p>
-        <span>Квартира:</span> <span>{{ address.flat }}</span>
-      </p>
+      <div v-if="address">
+        <p>
+          <span>Страна:</span> <span>{{ address.country }}</span>
+        </p>
+        <p>
+          <span>Город:</span> <span>{{ address.city }}</span>
+        </p>
+        <p>
+          <span>Улица:</span> <span>{{ address.street }}</span>
+        </p>
+        <p>
+          <span>Дом:</span> <span>{{ address.house }}</span>
+        </p>
+        <p>
+          <span>Квартира:</span> <span>{{ address.flat }}</span>
+        </p>
+      </div>
     </div>
 
-    <span>Человек:</span>
-    <search-input
-      :search-function="searchFunctionsList.getPersonSuggestions"
-      @value-selected="person = $event"
-    />
+    <div class="person">
+      <span>Человек:</span>
+      <search-input
+        :search-function="searchFunctionsList.getPersonSuggestions"
+        @value-selected="person = $event"
+      />
 
-    <div v-if="person">
-      <p>
-        <span>Имя:</span> <span>{{ person.name }}</span>
-      </p>
-      <p>
-        <span>Фамилия:</span> <span>{{ person.lastName }}</span>
-      </p>
+      <div v-if="person">
+        <p>
+          <span>Имя:</span> <span>{{ person.name }}</span>
+        </p>
+        <p>
+          <span>Фамилия:</span> <span>{{ person.lastName }}</span>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -73,9 +77,14 @@ export default class App extends Vue {
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  width 300px
+  width 700px
   color #2c3e50
   margin 0 auto
+  display flex
+  justify-content space-between
   & *
     box-sizing border-box
+  .address, .person
+    width 300px
+    max-width 300px
 </style>
